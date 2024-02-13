6b78ee47a2607620d45d6f4d415d5cd7b91d73e2
v59625yw <159910638+v59625yw@users.noreply.github.com>
2024-02-13 13:28:04 +0000

    Add files via upload

---
+++ hello_world.c
@@ -0,0 +1,11 @@
+#include <stdio.h> // directive to include 
+
+int main(void)   // function which is run when the programs starts 
+
+{ 
+
+printf("Hello World"); // our statement/command to run 
+
+    return 0; // the return statement for the function main 
+
+} 
